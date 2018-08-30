9163966b9273d931aa9045b1d4b0cc54693a43d5
yuliya <igordiye@caltech.edu>
2018-08-30 13:41:53 -0700

    added fast implementation

--- mp2_canonical_fast.py
+++ mp2_canonical_fast.py
@@ -5,7 +5,7 @@ from pyscf import gto, scf, fci
 """This module transforms AO integrals to MO basis
 Obtains RHF energy and MO coefficients from pyscf
 and calculates second order Moller Plesset energy
-Three SLOW implementations
+FAST implementation
 """
 
 mol = gto.M(
@@ -57,84 +57,18 @@ ehf = e
 #######################################################
 
 #Perform integral transformation for AO basis to MO basis
-def transform_integrals_nobby(num_bf, g2e_ao, coeff_mat):
-    "Noddy (O(N^8)) AO to MO integral transformation"
-
-    """
-    num_bf = number of basis functions
-    g2e_ao = two-electron integrals in AO basis
-    coeff_mat =  Coefficient matrix
-    """
-    start_time = time.clock()
-    g2e_mo = np.zeros(g2e_ao.shape)
-    for p in range(num_bf):
-        for q in range(num_bf):
-            for r in range(num_bf):
-                for s in range(num_bf):
-                    for mu in range(num_bf):
-                        for nu in range(num_bf):
-                            for rho in range(num_bf):
-                                for sigma in range(num_bf):
-                                    g2e_mo[p, q, r, s] += coeff_mat[p, mu]*coeff_mat[q, nu]*\
-                                    g2e_ao[mu, nu, rho, sigma]*\
-                                    coeff_mat[r, rho]*coeff_mat[s, sigma]
-    print(time.clock() - start_time, "seconds")
-    return g2e_mo
-
-# print(transform_integrals_nobby(num_bf, eri, coeff_mat) )
-
-
 def transform_integrals_einsum(ge2_ao, coeff_mat):
     """AO to MO integral transformation using einsum
     """
     start_time = time.clock()
-    g2e_mo = np.einsum('PQRS, Pp, Qq, Rr, Ss->pqrs', g2e_ao, coeff_mat, coeff_mat, coeff_mat, coeff_mat)
+    g2e_mo = np.einsum('PQRS, Pp->pQRS', g2e_ao, coeff_mat)
+    g2e_mo = np.einsum('pQRS, Qq->pqRS', g2e_ao, coeff_mat)
+    g2e_mo = np.einsum('pqRS, Rr->pqrS', g2e_ao, coeff_mat)
+    g2e_mo = np.einsum('pqrS, Ss->pqrs', g2e_ao, coeff_mat)
     print(time.clock() - start_time, "seconds")
     return g2e_mo
 
 # print(transform_integrals_einsum(eri, coeff_mat))
-
-
-def transform_integrals(num_bf, g2e_ao, coeff_mat):
-    """Integral transformation more efficiently O(N^5)
-    """
-    start_time = time.clock()
-    g = np.zeros(g2e_ao.shape)
-    for mu in range(num_bf):
-        for nu in range(num_bf):
-            for rho in range(num_bf):
-                for sigma in range(num_bf):
-                    for s in range(num_bf):
-                        g[mu, nu, rho, s] += g2e_ao[mu, nu, rho, sigma] * coeff_mat[rho, s]
-
-
-    gmo = np.zeros(g2e_ao.shape)
-    for mu in range(num_bf):
-        for nu in range(num_bf):
-            for r in range(num_bf):
-                for s in range(num_bf):
-                    for rho in range(num_bf):
-                        gmo[mu, nu, r, s] += g[mu, nu, rho, s]*coeff_mat[rho, r]
-
-    g.fill(0)
-    for mu in range(num_bf):
-        for q in range(num_bf):
-            for r in range(num_bf):
-                for s in range(num_bf):
-                    for nu in range(num_bf):
-                        g[mu, q, r, s] += gmo[mu, nu, r, s]*coeff_mat[nu, q]
-
-    gmo.fill(0)
-    for p in range(num_bf):
-        for q in range(num_bf):
-            for r in range(num_bf):
-                for s in range(num_bf):
-                    for mu in range(num_bf):
-                        gmo[p, q, r, s] += g[mu, q, r, s]*coeff_mat[mu, p]
-    print(time.clock() - start_time, "seconds")
-    return gmo
-
-# print(transform_integrals(num_bf, eri, coeff_mat))
 
 ##################################################
 #
@@ -142,8 +76,7 @@ def transform_integrals(num_bf, g2e_ao, coeff_mat):
 #
 ##################################################
 nocc = 1
-g2e_mo = transform_integrals(num_bf, g2e_ao, coeff_mat)
-# g2e_mo = transform_integrals_einsum(g2e_ao, coeff_mat)
+g2e_mo = transform_integrals_einsum(g2e_ao, coeff_mat)
 def compute_mp2_energy(num_bf, nocc, g2e_mo, orb_e, ehf):
     """
     Compute the MP2 energy
